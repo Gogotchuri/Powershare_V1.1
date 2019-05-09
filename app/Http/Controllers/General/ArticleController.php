4ba0c9ceccd5db484eb75a07685b3de962940de3
Gogotchuri <igogo17@freeuni.edu.ge>
2019-05-09 16:25:43 +0400

    implemented search and date sorting for articles

--- app/Http/Controllers/General/ArticleController.php
+++ app/Http/Controllers/General/ArticleController.php
@@ -10,11 +10,32 @@ use App\Http\Controllers\Controller;
 
 class ArticleController extends Controller
 {
-    public function index(){
-        //Should add is published to model
-        // and filter and sort by date
-        return ArticlesResource::collection(Article::all());
+    private const DEFAULT_PAGINATION = 10;
+
+    public function index(Request $request){
+        $query = Article::where("id", ">", -1)->orderBy("updated_at", "asc");
+
+        $name = $request["name"];
+        $pagination = $request["pagination"];
+
+        if($name !== null)
+            $query->where("name", "like", "%" . $name . "%");
+
+        //Default pagination if not provided
+        if($pagination === null)
+            $articles = $query->paginate(self::DEFAULT_PAGINATION);
+
+        else if(is_numeric($pagination) && $pagination <= 0) {
+            $articles = $query->get();
+
+            return self::responseData(ArticlesResource::collection($articles));
+        }else
+            $articles = $query->paginate($pagination);
+
+        return ArticlesResource::collection($articles);
     }
+
+
     public function show($id){
         $article = Article::where("id", $id)->first();
         if($article == null)
