e30ccc00882fc61f733111f7c4bea6ef6543b297
lashaloladze <l.loladze11@gmail.com>
2019-05-27 22:26:35 +0400

    added modal (needed style)

---
+++ resources/views/auth/LoginModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <mdb-btn @click.native="modal = true">Login</mdb-btn>
+    <mdb-modal :show="modal" @close="modal = false">
+      <mdb-modal-header>
+        <mdb-modal-title>Modal title</mdb-modal-title>
+      </mdb-modal-header>
+      <mdb-modal-body>
+          <login/>
+      </mdb-modal-body>
+      <mdb-modal-footer>
+        <mdb-btn color="secondary" @click.native="modal = false">Close</mdb-btn>
+        <mdb-btn color="primary">Save changes</mdb-btn>
+      </mdb-modal-footer>
+    </mdb-modal>
+  </div>
+</template>
+
+<script>
+    import login from "@views/auth/Login";
+    import { mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter, mdbBtn } from 'mdbvue';
+    export default {
+        components: {
+        mdbModal,
+        mdbModalHeader,
+        mdbModalTitle,
+        mdbModalBody,
+        mdbModalFooter,
+        mdbBtn,
+        login
+        },
+        data() {
+        return {
+            modal: false
+        };
+        }
+    };
+</script>
---
+++ resources/views/auth/RegisterModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <mdb-btn @click.native="modal = true">Register</mdb-btn>
+    <mdb-modal :show="modal" @close="modal = false">
+      <mdb-modal-header>
+        <mdb-modal-title>Modal title</mdb-modal-title>
+      </mdb-modal-header>
+      <mdb-modal-body>
+        <register/>
+      </mdb-modal-body>
+      <mdb-modal-footer>
+        <mdb-btn color="secondary" @click.native="modal = false">Close</mdb-btn>
+        <mdb-btn color="primary">Save changes</mdb-btn>
+      </mdb-modal-footer>
+    </mdb-modal>
+  </div>
+</template>
+
+<script>
+  import register from "@views/auth/Register";
+  import { mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter, mdbBtn } from 'mdbvue';
+  export default {
+    components: {
+      mdbModal,
+      mdbModalHeader,
+      mdbModalTitle,
+      mdbModalBody,
+      mdbModalFooter,
+      mdbBtn,
+      register
+    },
+    data() {
+      return {
+        modal: false
+      };
+    }
+  };
+</script>
