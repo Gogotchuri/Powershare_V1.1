77d32b526093b246642e79033ca771a73263b294
Gogotchuri <igogo17@freeuni.edu.ge>
2019-06-02 19:52:38 +0400

    Implemented edit, delete and view from panels. approving in progress

--- resources/views/shared/campaigns/Campaigns.vue
+++ resources/views/shared/campaigns/Campaigns.vue
@@ -19,17 +19,35 @@
                     <td v-else-if="campaign.status_id == 2">PROPOSED</td>
                     <td v-else>DRAFT</td>
                     <td v-if="atAdmin">
-                        <button style="color: yellow">EDIT</button>
-                        <button style="color: red">DELETE</button>
-                        <button v-if="campaign.status_id != 1" style="color: green">APPROVE</button>
-                        <button v-else style="color: blue">VIEW</button>
+                        <button style="color: yellow"
+                                @click="adminEdit(campaign.id)">
+                            EDIT
+                        </button>
+<!--                        TODO-->
+                        <button style="color: red"
+                                @click="adminDelete(campaign.id)">DELETE</button>
+                        <button v-if="campaign.status_id != 1" style="color: green"
+                                @click="approveCampaign(campaign.id)">APPROVE</button>
+                        <button v-else style="color: blue"
+                                @click="viewCampaign(campaign.id)">
+                            VIEW
+                        </button>
                     </td>
                     <td v-if="campaign.status_id == 1 && !atAdmin">
-                        <button style="color: green">VIEW</button>
+                        <button style="color: green"
+                                @click="viewCampaign(campaign.id)">
+                            VIEW
+                        </button>
                     </td>
                     <td v-else-if="!atAdmin">
-                        <button style="color: yellow">EDIT</button>
-                        <button style="color: red">DELETE</button>
+                        <button style="color: yellow"
+                                @click="userEdit(campaign.id)">
+                            EDIT
+                        </button>
+                        <button style="color: red"
+                                @click="userDelete(campaign.id)">
+                            DELETE
+                        </button>
                     </td>
                 </tr>
             </table>
@@ -69,7 +87,40 @@
         },
 
         methods:{
-
+            approveCampaign(id){
+                // this.campaigns.find()
+                //TODO implement me
+                HTTP.PUT("/admin/campaigns/"+id, this.campaigns[id])
+                    .then(() => window.alert("CampaignApproved"))
+                    .catch(r => console.error(r.response));
+            },
+            adminDelete(id){
+              HTTP.DELETE("/admin/campaigns/"+id)
+                  .then(() => {
+                    window.alert("Campaign has been deleted!");
+                      this.campaigns = this.campaigns.filter((c) => {
+                          return c.id !== id;
+                      });
+                  }).catch(r => console.error(r.response));
+            },
+            userDelete(id){
+                HTTP.DELETE("/user/campaigns/"+id)
+                    .then(() => {
+                        window.alert("Campaign has been deleted!");
+                        this.campaigns = this.campaigns.filter((c) => {
+                           return c.id !== id;
+                        });
+                    }).catch(r => console.error(r.response));
+            },
+            adminEdit(id){
+                this.$router.push('/admin/campaigns/'+id+'/edit')
+            },
+            viewCampaign(id){
+                this.$router.push('/campaigns/'+id);
+            },
+            userEdit(id){
+                this.$router.push('/user/campaigns/'+id+'/edit');
+            }
         }
     }
 </script>
