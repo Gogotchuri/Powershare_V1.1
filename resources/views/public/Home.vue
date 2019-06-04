9c002215aad8b953c04955665213dd43df4f6613
lashaloladze <l.loladze11@gmail.com>
2019-06-04 15:26:14 +0400

    added new text on landing (under construction)

--- resources/views/public/Home.vue
+++ resources/views/public/Home.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div  class="m-b-md">
+    <div class="flex-center position-ref full-height" style="text-align:center;">
+        <div class="content" style="width:100%;">
+            <div  class="m-b-md" style="margin-top:50px">
                 <h2 class="title m-b-md">
                     Powershare
                 </h2>
                 <h3>
                     Future of crowdfunding!
+                </h3>
+                <h3 style="margin-top:50px">
+                    under Construction
                 </h3>
             </div>
         </div>
