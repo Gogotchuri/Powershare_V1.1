2fb76b41390bfad249ec80500b0e94c001bc7a12
lashaloladze <l.loladze11@gmail.com>
2019-06-12 17:40:18 +0400

    designed new footer

--- resources/views/App.vue
+++ resources/views/App.vue
@@ -8,19 +8,21 @@
         <router-view/>
       </main>
     </div>
+    <footer-main/>
   </div>
 </template>
 
 <script>
   import MainNav from "@views/public/partials/MainNav";
   import AdminNav from "@/views/admin/partials/AdminNav";
+  import FooterMain from "@/views/public/partials/FooterMain";
 
   import {checkAdmin} from "@/js/Helpers/auth";
   import AdminIndex from "@views/admin/Index";
   import UserIndex from "@/views/user/Index";
 
   export default {
-    components: {UserIndex, AdminIndex, AdminNav, MainNav},
+    components: {UserIndex, AdminIndex, AdminNav, MainNav, FooterMain},
     computed:{
       isAdminPanel(){
         return this.$route.matched.some(r => r.meta.adminRequired);
