6f6f652b05746fef995d0bd0e2eb2f16c1a1437c
lashaloladze <l.loladze11@gmail.com>
2019-06-19 12:40:46 +0400

    added new detals on navigation bar

--- resources/views/public/partials/MainNav.vue
+++ resources/views/public/partials/MainNav.vue
@@ -16,13 +16,16 @@
                 <router-link :to="{ name: 'Articles' }" class="nav-link">Articles</router-link>
                 <login-modal v-if="!isLoggedIn"/>
                 <register-modal v-if="!isLoggedIn"/>
+                <router-link :to="{ name: 'User.Campaigns.Create' }" class="nav-link" id="create-link">Create Campaign</router-link>
                 <router-link
                         :to="{ name: 'User.Profile' }"
                         class="nav-link"
                         v-if="currentUser"
-                >Profile of {{currentUser.name}}</router-link>
-                <a href="#" @click.prevent="logout" class="nav-link" v-if="isLoggedIn">LogOut</a>
-                <router-link :to="{ name: 'User.Campaigns.Create' }" class="nav-link" id="create-link">Create Campaign</router-link>
+                >Profile</router-link>
+                <a href="#" @click.prevent="logout" class="nav-link" v-if="isLoggedIn">
+                    <img src="img/log-out.svg" class="log-out-icon">
+                    <div class="logout-on-phone">Logout</div>
+                </a>
             </ul>
         </div>
     </nav>
