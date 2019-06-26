5f16d8e8b4c1e47c55e89bbc742e35316b17e43e
lashaloladze <l.loladze11@gmail.com>
2019-06-26 12:43:50 +0400

    changes on details page. georgian texts are added, some margins are fixed

--- resources/views/public/Campaign.vue
+++ resources/views/public/Campaign.vue
@@ -13,7 +13,7 @@
             <p class="header-text">{{campaign.name}}</p>
             <p class="header-date">{{campaign.date}}</p>
           </div>
-          <p class="header-left">Due to: {{campaign.due_date}}</p>
+          <p class="header-left">ძალაშია: {{campaign.due_date}}</p>
         </div>
         <div class="owner">
           <div class="owner-icon">
@@ -24,13 +24,13 @@
               <a href="#">
                 <!-- change with svg -->
                 <img src="/img/share-2.svg" alt="">
-                Share
+                გააზიარე
               </a>
               <a v-if="isLoggedIn" style="cursor:pointer;" @click="changeFavouriteStatus">
                 <!-- change with svg -->
                 <img v-if="favourite" src="/img/heart-red.svg" alt="">
                 <img v-else src="/img/heart.svg" alt="">
-                Save
+                შეინახე
               </a>
             </span>
           </div>
@@ -40,24 +40,24 @@
         </div>
       </div>
       <div class="completion">
-        <p class="completion-donators">{{campaign.num_surveys_filled}} free donators</p>
+        <p class="completion-donators">{{campaign.num_surveys_filled}} მოხალისე მხარდამჭერი</p>
         <div class="fillable-bar">
           <div class="filled-bar"></div>
         </div>
-        <span>{{campaign.required_funding}}$<br> needed</span>
+        <span>{{campaign.required_funding}}$<br> საჭიროა</span>
       </div>
       <div class="about hided-on-ms">
-        <p class="about-header">About the campaign</p>
+        <p class="about-header">კამპანის შესახებ</p>
         <p class="about-content"> {{campaign.details}}</p>
       </div>
       <div class="gallery hided-on-ms">
-        <p class="gallery-header">Gallery</p>
+        <p class="gallery-header">გალერეა</p>
         <div class="gallery-content" v-if="gallery">
           <gallery-modal v-for="image in gallery" v-bind:key="image.id" :campaign_photo_url="image.url" ></gallery-modal>
         </div>
       </div>
       <div class="comments hided-on-ms">
-        <p class="comments-header">Comments</p>
+        <p class="comments-header">კომენტარები</p>
         <div class="comment" v-for="comment in campaign.comments" v-bind:key="comment.id">
           <div class="icon"></div>
           <p class="comment-name">{{comment.author_name}}</p>
@@ -71,14 +71,14 @@
         </p>
         </div>
     <div class="gallery hided-on-l">
-        <p class="gallery-header">Gallery</p>
+        <p class="gallery-header">გალერეა</p>
         <div class="gallery-content" v-if="gallery">
           <gallery-modal></gallery-modal>
           <div v-for="image in gallery" :style="{'background-image': 'url(' + image.url + ')'}"></div>
         </div>
     </div>
     <div class="comments hided-on-l">
-      <p class="comments-header">Comments</p>
+      <p class="comments-header">კომენტარები</p>
         <div class="comment" v-for="comment in campaign.comments" v-bind:key="comment.id">
           <div class="icon"></div>
           <p class="comment-name">{{comment.author_name}}</p>
@@ -94,13 +94,13 @@
           <a href="#">
           <!-- change with svg -->
             <img src="/img/share-2.svg" alt="">
-                  Share
+                  გააზიარე
           </a>
           <a v-if="isLoggedIn" style="cursor:pointer;" @click="changeFavouriteStatus">
           <!-- change with svg -->
             <img v-if="favourite" src="/img/heart-red.svg" alt="">
             <img v-else src="/img/heart.svg" alt="">
-                Save
+                შეინახე
           </a>
         </span>
         <donation-modal :campaign_id="campaign.id" :is_logged_in="isLoggedIn"/>
