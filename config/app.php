3ff9dfa4a2feaa9c96cb4024abb902fb80d38624
Gogotchuri <igogo17@freeuni.edu.ge>
2019-07-03 21:25:48 +0400

    Added prerendering for crawler, still testing

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Nutsweb\LaravelPrerender\LaravelPrerenderServiceProvider::class,
     ],
 
     /*
